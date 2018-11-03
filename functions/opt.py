edd876f20d4beef1ac82f46f53cf171625418e67
nex3z <litianxing9@gmail.com>
2018-11-03 20:58:00 +0800

    Update opt.py.

--- functions/opt.py
+++ functions/opt.py
@@ -2,7 +2,7 @@ from functions.diff import numerical_gradient
 
 
 def gradient_decent(f, initial_x, learning_rate=0.01, num_step=100):
-    x = initial_x
+    x = initial_x.copy()
     for i in range(num_step):
         grad = numerical_gradient(f, x)
         x -= learning_rate * grad
