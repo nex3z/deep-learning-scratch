a505bc66d1bb302d7a461c6e6fc1863fe9e0e32e
nex3z <litianxing9@gmail.com>
2018-11-12 21:52:24 +0800

    Add functions.py.

---
+++ learn/functions.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+
+def step_function(x):
+    return np.array(x > 0, dtype=np.int)
+
+
+def sigmoid(x):
+    return 1.0 / (1.0 + np.exp(-x))
+
+
+def softmax(x):
+    if x.ndim == 1:
+        x = x.reshape(1, len(x))
+    x = x - np.max(x, axis=1, keepdims=True)
+    return np.exp(x) / np.sum(np.exp(x), axis=1, keepdims=True)
+
+
+def relu(x):
+    return np.maximum(0, x)
+
+
+def mean_square_error(y, y_hat):
+    return 0.5 * np.sum(np.square(y - y_hat))
+
+
+def cross_entropy_error(y, y_hat):
+    if y.ndim == 1:
+        y = y.reshape(1, len(y))
+        y_hat = y_hat.reshape(1, len(y_hat))
+    batch_size = y.shape[0]
+    return -np.sum(y_hat * np.log(y + 1e-7)) / batch_size
