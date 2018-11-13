d3415892adcbc9cf99ed0a946f452cd033facaba
nex3z <litianxing9@gmail.com>
2018-11-13 21:53:53 +0800

    Add layers.py.

---
+++ learn/layers.py
@@ -0,0 +1,101 @@
+import numpy as np
+from learn.functions import sigmoid, softmax, cross_entropy_error
+
+
+class Add(object):
+    @staticmethod
+    def forward(x, y):
+        return x + y
+
+    @staticmethod
+    def backward(dout):
+        dx = dout * 1
+        dy = dout * 1
+        return dx, dy
+
+
+class Multiply(object):
+    def __init__(self):
+        self.x = None
+        self.y = None
+
+    def forward(self, x, y):
+        self.x = x
+        self.y = y
+        return x * y
+
+    def backward(self, dout):
+        dx = dout * self.y
+        dy = dout * self.x
+        return dx, dy
+
+
+class Dense(object):
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.original_x_shape = None
+        self.dW = None
+        self.db = None
+
+    def forward(self, x):
+        self.original_x_shape = x.shape
+        x = x.reshape(x.shape[0], -1)
+        self.x = x
+        out = np.dot(self.x, self.W) + self.b
+        return out
+
+    def backward(self, dout):
+        dx = dout.dot(self.W.T)
+        self.dW = self.x.T.dot(dout)
+        self.db = np.sum(dout, axis=0)
+        dx = dx.reshape(*self.original_x_shape)
+        return dx
+
+
+class Relu(object):
+    def __init__(self):
+        self.mask = None
+
+    def forward(self, x):
+        self.mask = (x <= 0)
+        out = x.copy()
+        out[self.mask] = 0
+        return out
+
+    def backward(self, dout):
+        dout[self.mask] = 0
+        dx = dout
+        return dx
+
+
+class Sigmoid(object):
+    def __init__(self):
+        self.out = None
+
+    def forward(self, x):
+        self.out = sigmoid(x)
+        return self.out
+
+    def backward(self, dout):
+        dx = dout * (1.0 - self.out) * self.out
+        return dx
+
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None
+        self.y = None
+        self.t = None
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
+        return dx
