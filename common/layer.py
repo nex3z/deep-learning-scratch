5f4e55e37c1963ee03cdfa86a8beca2e8c61d9ed
nex3z <litianxing9@gmail.com>
2019-01-10 00:18:12 +0800

    Add conv net.

--- common/layer.py
+++ common/layer.py
@@ -2,6 +2,7 @@ import numpy as np
 from common.function import sigmoid, softmax, cross_entropy_error
 from common.initializer import normal, zeros
 from common.exception import UnimplementedMethodException
+from common.util import col2im, im2col
 
 
 class BaseLayer(object):
@@ -124,12 +125,18 @@ class SoftmaxWithLoss(BaseLayer):
     def forward(self, x, **kwargs):
         self.y = kwargs['y']
         self.y_hat = softmax(x)
-        self.loss = cross_entropy_error(self.y_hat, self.y)
+        self.loss = cross_entropy_error(self.y, self.y_hat)
         return self.loss
 
     def backward(self, dout=1):
         batch_size = self.y.shape[0]
-        dx = (self.y_hat - self.y) / batch_size
+        is_one_hot = self.y.size == self.y_hat.size
+        if is_one_hot:
+            dx = (self.y_hat - self.y) / batch_size
+        else:
+            dx = self.y_hat.copy()
+            dx[np.arange(batch_size), self.y] -= 1
+            dx = dx / batch_size
         return dx
 
 
@@ -233,3 +240,104 @@ class BatchNorm(BaseLayer):
             'gamma': self.dgamma,
             'beta': self.dbeta,
         }
+
+
+class Conv2D(BaseLayer):
+    def __init__(self, input_size, filters, kernel_size=(3, 3), stride=1, padding=0, kernel_initializer=normal,
+                 bias_initializer=zeros):
+        self.W = kernel_initializer(filters, input_size[0], *kernel_size)
+        self.b = bias_initializer(filters)
+        self.dW = None
+        self.db = None
+
+        self.stride = stride
+        self.pad = padding
+
+        self.x = None
+        self.col = None
+        self.col_W = None
+
+    def forward(self, x, **kwargs):
+        kernel_n, kernel_c, kernel_h, kernel_w = self.W.shape
+        x_n, x_c, x_h, x_w = x.shape
+        out_h = 1 + int((x_h + 2 * self.pad - kernel_h) / self.stride)
+        out_w = 1 + int((x_w + 2 * self.pad - kernel_w) / self.stride)
+
+        col = im2col(x, kernel_h, kernel_w, self.stride, self.pad)
+        col_W = self.W.reshape(kernel_n, -1).T
+
+        out = np.dot(col, col_W) + self.b
+        out = out.reshape(x_n, out_h, out_w, -1).transpose(0, 3, 1, 2)
+
+        self.x = x
+        self.col = col
+        self.col_W = col_W
+
+        return out
+
+    def backward(self, dout):
+        kernel_n, kernel_c, kernel_h, kernel_w = self.W.shape
+        dout = dout.transpose((0, 2, 3, 1)).reshape(-1, kernel_n)
+
+        self.db = np.sum(dout, axis=0)
+        self.dW = np.dot(self.col.T, dout)
+        self.dW = self.dW.transpose((1, 0)).reshape(kernel_n, kernel_c, kernel_h, kernel_w)
+
+        dcol = np.dot(dout, self.col_W.T)
+        dx = col2im(dcol, self.x.shape, kernel_h, kernel_w, self.stride, self.pad)
+
+        return dx
+
+    @property
+    def params(self):
+        return {
+            'W': self.W,
+            'b': self.b,
+        }
+
+    @property
+    def dparams(self):
+        return {
+            'W': self.dW,
+            'b': self.db,
+        }
+
+
+class MaxPool2D(BaseLayer):
+    def __init__(self, pool_size, stride=1, padding=0):
+        self.pool_h, self.pool_w = pool_size
+        self.stride = stride
+        self.pad = padding
+
+        self.x = None
+        self.arg_max = None
+
+    def forward(self, x, **kwargs):
+        n, c, h, w = x.shape
+        out_h = int(1 + (h - self.pool_h) / self.stride)
+        out_w = int(1 + (w - self.pool_w) / self.stride)
+
+        col = im2col(x, self.pool_h, self.pool_w, self.stride, self.pad)
+        col = col.reshape(-1, self.pool_h * self.pool_w)
+
+        arg_max = np.argmax(col, axis=1)
+        out = np.max(col, axis=1)
+        out = out.reshape(n, out_h, out_w, c).transpose((0, 3, 1, 2))
+
+        self.x = x
+        self.arg_max = arg_max
+
+        return out
+
+    def backward(self, dout):
+        dout = dout.transpose((0, 2, 3, 1))
+
+        pool_size = self.pool_h * self.pool_w
+        dmax = np.zeros((dout.size, pool_size))
+        dmax[np.arange(self.arg_max.size), self.arg_max.flatten()] = dout.flatten()
+        dmax = dmax.reshape(dout.shape + (pool_size,))
+
+        dcol = dmax.reshape(dmax.shape[0] * dmax.shape[1] * dmax.shape[2], -1)
+        dx = col2im(dcol, self.x.shape, self.pool_h, self.pool_w, self.stride, self.pad)
+
+        return dx
---
+++ common/util.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+
+def im2col(input_data, filter_h, filter_w, stride=1, padding=0):
+    n, c, h, w = input_data.shape
+    out_h = (h + 2 * padding - filter_h) // stride + 1
+    out_w = (w + 2 * padding - filter_w) // stride + 1
+
+    img = np.pad(input_data, [(0, 0), (0, 0), (padding, padding), (padding, padding)], 'constant')
+    col = np.zeros((n, c, filter_h, filter_w, out_h, out_w))
+
+    for y in range(filter_h):
+        y_max = y + stride * out_h
+        for x in range(filter_w):
+            x_max = x + stride*out_w
+            col[:, :, y, x, :, :] = img[:, :, y:y_max:stride, x:x_max:stride]
+
+    col = col.transpose((0, 4, 5, 1, 2, 3)).reshape(n * out_h * out_w, -1)
+    return col
+
+
+def col2im(col, input_shape, filter_h, filter_w, stride=1, padding=0):
+    n, c, h, w = input_shape
+    out_h = (h + 2 * padding - filter_h) // stride + 1
+    out_w = (w + 2 * padding - filter_w) // stride + 1
+    col = col.reshape(n, out_h, out_w, c, filter_h, filter_w).transpose((0, 3, 4, 5, 1, 2))
+
+    img = np.zeros((n, c, h + 2 * padding + stride - 1, w + 2 * padding + stride - 1))
+    for y in range(filter_h):
+        y_max = y + stride * out_h
+        for x in range(filter_w):
+            x_max = x + stride * out_w
+            img[:, :, y:y_max:stride, x:x_max:stride] += col[:, :, y, x, :, :]
+
+    return img[:, :, padding:h + padding, padding:w + padding]
